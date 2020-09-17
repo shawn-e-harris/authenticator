8d6d0bcc0b6a38e22de037d777315ed9b4d9ef0c
Shawn <shawn.harris3510@gmail.com>
2020-09-17 19:31:13 -0400

    first commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+users = [
+    {username: "shawnH", password: "developer",}
+    {username: "rickSanchez", password: "burp",}
+    {username: "harryPotter", password: "slytherin",}
+    {username: "blackPanther", password: "wakandaForever",}
+    {username: "vegeta", password: "saiyanPrince",}
+]
+
+puts "Welcome to the Authenticator"
+25.times {print "_"}
+puts
+puts "This program will take input from the user and compare password"
+puts "If the password is correct, you will get back the user object"
---
+++ readMe.md
@@ -0,0 +1,3 @@
+Welcome to the authenticator!
+
+This program will take input from the user and compare password input from user when run in the terminal
